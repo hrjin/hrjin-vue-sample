56a786fcaebd6fe70ab57458a2de6a4302cb2a80
hrjin <hyunlee3jdh@gmail.com>
2021-03-22 17:17:31 +0900

    vue.js 예제 따라하기 - Layout 기본 설정

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <Header />
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "./components/layout/Header";
+import HelloWorld from './components/HelloWorld.vue'
+import Footer from "./components/layout/Footer";
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    HelloWorld,
+    Footer
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+    <footer>
+        <p>CopyRightⓒ hrjin</p>
+    </footer>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    footer{border-top: 1px solid #35495e; text-align: center; font-size: 16px; color: #42b983; margin: 100px 0 0 0;}
+</style>
